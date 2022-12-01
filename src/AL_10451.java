704a129575c00a098c12ab90cb32e4dc4813505f
Bigfish4tim <bigfish.4tim@gmail.com>
2022-12-01 17:47:43 +0900

    12-01

--- src/AL_10451.java
+++ src/AL_10451.java
@@ -1,7 +1,57 @@
+import java.io.*;
 import java.util.*;
 
 public class AL_10451 {
-    public static void main(String[] args) {
+    static int T;
+    static int N;
+    static int[] map;
+    static int count;
 
+    public static int find(int x) {
+        if(map[x] == x) return x;
+
+        return find(map[x]);
+    }
+
+    public static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if(x==y) {
+            count++;
+        } else if(x>y) {
+            map[x] = y;
+        } else {
+            map[y] = x;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        T = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<T; i++) {
+            N = Integer.parseInt(br.readLine());
+
+            map = new int[N+1];
+            count = 0;
+
+            st = new StringTokenizer(br.readLine());
+            for(int j=1; j<N+1; j++) {
+                map[j] = j;
+            }
+
+            for(int j=1; j<N+1; j++) {
+                int b = Integer.parseInt(st.nextToken());
+                union(j, b);
+            }
+            bw.write(count + "\n");
+        }
+
+        bw.flush();
+        bw.close();
     }
 }

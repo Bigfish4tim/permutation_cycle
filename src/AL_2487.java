9b7e4f1b8e782f328b5050d1be2cbcd69739d8c7
Bigfish4tim <bigfish.4tim@gmail.com>
2022-12-05 17:15:54 +0900

    12-05

--- src/AL_2487.java
+++ src/AL_2487.java
@@ -7,8 +7,8 @@ public class AL_2487 {
     static int N;
     static int[] map;
     static int[] counts;
-    static int count;
     static int ans;
+    static boolean[] visit;
 
     public static int gcd(int x, int y) {
         if(y == 0) return x;
@@ -21,17 +21,15 @@ public class AL_2487 {
 
     public static int findCount(int x) {
         int mapx = map[x];
+        int count = 1;
 
         while (true) {
-            if(mapx != x) {
-                mapx = map[mapx];
-                count++;
-            } else {
-                break;
-            }
+            if(x == mapx)
+                return count;
+            visit[mapx] = true;
+            mapx = map[mapx];
+            count++;
         }
-
-        return count;
     }
 
     public static void main(String[] args) throws IOException {
@@ -40,6 +38,7 @@ public class AL_2487 {
         N = Integer.parseInt(br.readLine());
         map = new int[N+1];
         counts = new int[N+1];
+        visit = new boolean[N+1];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=1; i<N+1; i++) {
@@ -47,20 +46,11 @@ public class AL_2487 {
             map[i] = Integer.parseInt(st.nextToken());
         }
 
+        ans = 1;
         for(int i=1; i<N+1; i++) {
-            count = 1;
-            counts[i] = findCount(i);
+            if(visit[i]) continue;
+            ans = lcd(ans, findCount(i));
         }
-
-        counts = new int[] {0, 3, 1, 3, 3, 3, 2};
-
-        ans = counts[1];
-
-
-        for(int i=2; i<N+1; i++) {
-            ans = lcd(ans, counts[i]);
-        }
-        System.out.println(Arrays.toString(counts));
 
         System.out.println(ans);
 
